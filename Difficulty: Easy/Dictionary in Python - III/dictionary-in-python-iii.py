18fcccbc5eb4178f9ada9080faaea4aaf69fc826
UNNATI KUMARI <unnatimishra891@gmail.com>
2026-07-12 22:55:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Dictionary in Python - III/dictionary-in-python-iii.py
@@ -0,0 +1,21 @@
+# insert into dictionary
+def insert_dict(query, dict):
+    # Your code here
+    dict[query[1]] = query[2]
+
+# deleting from dictionary
+def del_dict(query, dict):
+    # Your code here
+    if query[1] in dict:
+        del dict[query[1]]
+        return True
+    return False
+
+    
+# print marks of required name
+def print_dict(key, dict):
+    # Your code here
+    if key in dict:
+        print("Marks of", key, "is", dict[key])
+        return True
+    return False
