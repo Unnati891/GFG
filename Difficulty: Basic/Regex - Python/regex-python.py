4d5111e48b4accec435ee4b61606cc6a1a67aacd
UNNATI KUMARI <unnatimishra891@gmail.com>
2026-07-13 23:51:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Regex - Python/regex-python.py
@@ -0,0 +1,11 @@
+import re
+
+def numberMatcher(str):
+    pat = r"\d+"
+    match = re.findall(pat, str)
+
+    if match:
+        for i in match:
+            print(i, end=" ")
+    else:
+        print(-1, end="")
