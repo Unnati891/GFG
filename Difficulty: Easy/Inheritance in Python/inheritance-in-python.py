aa05aa86e0d149c30e3d433d4ce398d7fa645c24
UNNATI KUMARI <unnatimishra891@gmail.com>
2026-07-22 22:27:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Inheritance in Python/inheritance-in-python.py
@@ -0,0 +1,8 @@
+class Employee:
+    def __init__(self,id,salary):
+        self.id=id
+        self.salary=salary
+class SalesEmployee(Employee):
+    def __init__(self,id,salary,sales):
+        super().__init__(id,salary)
+        self.sales=sales
