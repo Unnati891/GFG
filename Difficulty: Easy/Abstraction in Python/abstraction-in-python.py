6f02ff164e9e6578ad6f0615c2bfb461ab79f09b
UNNATI KUMARI <unnatimishra891@gmail.com>
2026-07-23 23:47:22 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Abstraction in Python/abstraction-in-python.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod 
+class Shape(ABC):
+    def __init__(self,c):
+        self.color=c
+    def get_color(self):
+        return self.color
+        
+    @abstractmethod
+    
+    def get_area(self):
+        pass
+        
+class Square(Shape):
+    def __init__(self,c,side):
+        super().__init__(c)
+        self.side=side
+        
+        
+    def get_area(self):
+        return self.side*self.side
